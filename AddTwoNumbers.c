893f19391a97e2306b07d1e483f5d963c75ee4ac
Gopal6205 <93756473+Gopal6205@users.noreply.github.com>
2022-05-05 11:20:57 +0530

    Add files via upload

---
+++ AddTwoNumbers.c
@@ -0,0 +1,14 @@
+//WAP in C-Language To Print Addition of Two Numbers.
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\nEnter First Numbers");
+    scanf("%d,%d",&a);
+     printf("\nEnter Second Numbers");
+    scanf("%d,%d",&b);
+    c=a+b;
+    printf("\nAddition=%d",c);
+    return 0;
+}
+//Created__By____Gopal___Kumar__B.Tech(CSE)
